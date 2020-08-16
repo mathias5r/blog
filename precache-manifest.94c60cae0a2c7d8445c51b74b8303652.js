97e70b1743caac8f12985e9ed02fa2b0d4b74660
Mathias <mathias.silva@agriness.com>
2020-08-16 11:44:09 -0300

    Updates

---
+++ precache-manifest.94c60cae0a2c7d8445c51b74b8303652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7ae124291057d1057c6138dc9baef4",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "9c2755d5c9de5f822340",
+    "url": "/blog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ddbd33f2ce09a585915a",
+    "url": "/blog/static/js/2.022bd151.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog/static/js/2.022bd151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2755d5c9de5f822340",
+    "url": "/blog/static/js/main.c54e49bb.chunk.js"
+  },
+  {
+    "revision": "835233743d855939bee7",
+    "url": "/blog/static/js/runtime-main.10c1e3dd.js"
+  }
+]);
